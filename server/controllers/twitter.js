3cff40a0429e91c0dc9ee9fa8815ef071a6312bb
Nick Barry <niko.barry@gmail.com>
2016-07-18 11:23:08 -0700

    Squashed commit of the following:
    commit ad49a98d90808b5e729bbadeb0adff20d0911dc2
    Author: Nick Barry <niko.barry@gmail.com>
    Date:   Mon Jul 18 11:11:05 2016 -0700
    
        style: Style changes per styleguide
    
    commit f3bd6912a269989607cf225075b66b493837a5f8
    Author: Nick Barry <niko.barry@gmail.com>
    Date:   Mon Jul 18 09:56:46 2016 -0700
    
        style: Cleanup per style guide
    
    commit 8197647e55b27aa884680660ea706ffc8da107c2
    Author: Nick Barry <niko.barry@gmail.com>
    Date:   Mon Jul 18 09:45:34 2016 -0700
    
        feat (APIs): [WIP] Add gmail integration
    
        Connect to a gmail account and request a batch of messages. Work in progress.
    
    commit 4e6860e462b3e0f10cec11ba282a71de3ba019ac
    Author: John Slape <slape.john@gmail.com>
    Date:   Wed Jul 13 14:50:24 2016 -0700
    
        feat: only retrieve tweets after most recent in database
    
        Edit controllers/twitter.js to query the database, finding the tweet
        with the greatest since_id (tweet IDs increment on each tweet), then
        query the twitter API using this since_id to retrieve only tweets we
        have not yet stored.
    
        Closes #37, #107

--- server/controllers/twitter.js
+++ server/controllers/twitter.js
@@ -12,49 +12,48 @@ let client;
   });
 }
 
-//function errorCallback(err) {
-//  if (err) {
-//    console.log(err);
-//  }
-//}
-
-function cacheTweets(username, sinceId) {
-  //prepend all usernames with %40, replacing the @ symbol if provided
-  username = username.replace(/^(@|%40)?/, '%40');
-  const query = { q: username, since_id: sinceId };
-
-  client.get('search/tweets', query, function (error, tweets) {
-    if (error) {
-      console.log('Error retrieving tweets: ', error);
-    } else {
-      //console.log('Tweets returned from Twitter module: ', tweets);
-      tweets = tweets.statuses;
-      DbTweet.find().exec(function (err /* , cachedTweets */) {
-        if (err) {
-          console.error(err);
-        }
-
-        // console.log('tweets is', cachedTweets)
-      });
-
-      //for (let t of tweets) {
-      //  //const tweet = new DbTweet(t);
-      //  //tweet.save(errorCallback);
-      //
-      //  // DbTweet.findOneAndUpdate({ id_str: t.id_str }, t, { upsert: true })
-      //  //   .exec(errorCallback);
-      //
-      //  //if (DbTweet.findOne({id_str: t.id_str})) {
-      //  //  //console.log("skipping", t)
-      //  //  continue;
-      //  //}
-      //
-      //}
-    }
-  });
+function errorCallback(err) {
+  if (err) {
+    console.log(err);
+  }
 }
 
-cacheTweets('makersquare');
+function cacheTweets(username) {
+
+  //get most recent since_id
+
+  DbTweet.findOne({'text': {'$regex': username, '$options': 'i'}})
+  .sort('-id_str')
+  .exec(function(err, mostRecentTweet) {
+    let sinceId;
+    if (mostRecentTweet) {
+      sinceId = mostRecentTweet.id_str;
+    }
+    //prepend all usernames with %40, replacing the @ symbol if provided
+
+    username = username.replace(/^(@|%40)?/, '%40');
+    const query = { q: username, since_id: sinceId };
+
+      client.get('search/tweets', query, function (error, tweets) {
+      if (error) {
+        console.log('Error retrieving tweets: ', error);
+      } else {
+        // console.log('Tweets returned from Twitter module: ', tweets);
+        tweets = tweets.statuses;
+        for (let t of tweets) {
+          const tweet = new DbTweet(t);
+          tweet.save(errorCallback);
+        }
+      }
+    });
+  });
+
+}
+
+ //TODO: move this initial caching to a more appropriate place in the code.
+const username = 'makersquare';
+cacheTweets(username);
+
 function findDbTweets(queryObj) {
   return DbTweet.find(queryObj).exec();
 }
